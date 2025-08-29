e162046e2812686e4e9d3b08227cec2490c102c6
Afreed04-ops <afreeds975@gmail.com>
2025-08-29 15:13:00 +0530

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,11 +1,17 @@
 public class Calculator
 {
 public int add(int a,int b)
-    {return a+b;}
+    { 
+        return a+b;
+    }
 public int subtract(int a,int b)
-    {return a-b;}
+    {
+        return a-b;
+    }
 public int multiply(int a,int b)
-    {return a*b;}
+    {
+        return a*b;
+    }
 public int divide(int a, int b) 
     {
     if (b == 0) 
@@ -13,7 +19,6 @@ public int divide(int a, int b)
         throw new ArithmeticException("Cannot divide by zero");
     }
     return a / b;
-}
 }
 public static void main(String args[])
 {
