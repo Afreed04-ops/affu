260967682802e7e0f32068acbebf0028ca34d899
Afreed04-ops <afreeds975@gmail.com>
2025-08-29 15:07:53 +0530

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -2,11 +2,11 @@ public class Calculator{
 public int add(int a,int b){return a+b;}
 public int subtract(int a,int b){return a-b;}
 public int multiply(int a,int b){return a*b;}
-public int divide(int a,int b){if (b==0) throw zero arthmatic exception ("cannot divide by zero");
+public int divide(int a,int b){if (b==0) throw  ArthimethicException ("cannot divide by zero");
 return a/b;}
 public static void main(String args[])
 {
-calculator cal=new Calculator();
+Calculator cal=new Calculator();
 System.out.println("addition"+cal.add(10,5));
 System.out.println("subtraction"+cal.subtract(10,5));
 System.out.println("multiplication"+cal.multiply(10,5));
