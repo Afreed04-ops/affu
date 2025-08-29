a7131f76833a2bb9bc4121d633a61a2a71c6ba80
Afreed04-ops <afreeds975@gmail.com>
2025-08-29 15:10:02 +0530

    Update CalculatorTest.java

--- CalculatorTest.java
+++ CalculatorTest.java
@@ -1,10 +1,26 @@
-import org.junit.test;
+import org.junit.Test;  
 import static org.junit.Assert.assertEquals;
 
-public class CalculatorTest{
-Calculator cal=new Calculator();
-@Test public void testAddition(){assert Equals(15,cal.add(10,5));}
-@Test public void testSubtraction(){assert Equals(5,cal.substract(10,5));}
-@Test public void testMultiplication(){assert Equals(50,multiply(10,5));}
-@Test public void testDivision(){assert Equals(2,cal.divide(10,5));}
+public class CalculatorTest {
+    Calculator cal = new Calculator(); 
+
+    @Test 
+    public void testAddition() {
+        assertEquals(15, cal.add(10, 5));  
+    }
+
+    @Test 
+    public void testSubtraction() {
+        assertEquals(5, cal.subtract(10, 5));  
+    }
+
+    @Test 
+    public void testMultiplication() {
+        assertEquals(50, cal.multiply(10, 5));  
+    }
+
+    @Test 
+    public void testDivision() {
+        assertEquals(2, cal.divide(10, 5));  
+    }
 }
